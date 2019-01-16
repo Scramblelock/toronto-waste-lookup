a19c4cc2a09e722ea47c25bfd9cc3510edb3586b
Marc Sakalauskas <Marc@Marcs-iMac.local>
2019-01-16 11:34:08 -0500

    added hidden label to form  for greater accessibility

--- src/components/WasteSearchForm/WasteSearchForm.js
+++ src/components/WasteSearchForm/WasteSearchForm.js
@@ -2,20 +2,23 @@ import React from 'react';
 import './WasteSearchForm.css';
 import magnifyingGlass from './icons8-search-60.png';
 
-const WasteSearchForm = ({ onButtonSubmit, handleChange }) => {
-	return (
-		<form className='form' type='submit' onSubmit={onButtonSubmit}>
-      <input 
-      	type='text'
-      	placeholder='Enter a keyword'
-      	required
+const WasteSearchForm = ({onButtonSubmit, handleChange}) => {
+  return (
+    <form className="form" type="submit" onSubmit={onButtonSubmit}>
+      <label htmlFor="search" className="visuallyhidden">
+        Search: 
+      </label>
+      <input
+        type="text"
+        placeholder="Enter a keyword"
+        required
         onChange={handleChange}
       />
-      <button className='grow shadow-5'>
-      	<img src={magnifyingGlass} alt='magnifyingGlass'/>
+      <button className="grow shadow-5" aria-label="searchbutton">
+        <img src={magnifyingGlass} alt="magnifyingGlass" />
       </button>
     </form>
-  );  
-}
+  );
+};
 
 export default WasteSearchForm;
