9f2c1eb5ee363b7d9bbbef445b1f464fc35376c8
Marc Sakalauskas <Marc@Marcs-iMac.local>
2019-01-16 12:26:33 -0500

    Updates

---
+++ precache-manifest.22d55d4c9958012af11f2679546e537f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8367de1a80032da8e12cd32d2233556f",
+    "url": "/toronto-waste-lookup/static/media/icons8-search-60.8367de1a.png"
+  },
+  {
+    "revision": "b8bf0a16a6b4be6bcb34",
+    "url": "/toronto-waste-lookup/static/js/runtime~main.b8bf0a16.js"
+  },
+  {
+    "revision": "5471087fdf973fa814d2",
+    "url": "/toronto-waste-lookup/static/js/main.5471087f.chunk.js"
+  },
+  {
+    "revision": "532fa90baf57cc03012c",
+    "url": "/toronto-waste-lookup/static/js/1.532fa90b.chunk.js"
+  },
+  {
+    "revision": "5471087fdf973fa814d2",
+    "url": "/toronto-waste-lookup/static/css/main.76570dbc.chunk.css"
+  },
+  {
+    "revision": "532fa90baf57cc03012c",
+    "url": "/toronto-waste-lookup/static/css/1.eafa2819.chunk.css"
+  },
+  {
+    "revision": "4be559931e848ddae0ad79f9574fe2e4",
+    "url": "/toronto-waste-lookup/index.html"
+  }
+];
